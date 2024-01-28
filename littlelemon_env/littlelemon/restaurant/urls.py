b74001c1bb28b13c838b9394e1a5f487927e7f8f
IvanTPraveen <ivan.t.praveen@gmail.com>
2024-01-27 18:37:26 -0700

    added lines of code

--- littlelemon_env/littlelemon/restaurant/urls.py
+++ littlelemon_env/littlelemon/restaurant/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 #url paths under this sentence
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
